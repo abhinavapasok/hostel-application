2cf5465ec953bfc09f68b0ca2fb1d10b6a09ad54
Abhinav A P <asok0968@gmail.com>
2023-11-21 11:25:57 +0530

    added navbar-mobile-ui

--- hostel-frontend/src/Components/Navbar.tsx
+++ hostel-frontend/src/Components/Navbar.tsx
@@ -1,11 +1,23 @@
+import { ClassNames } from "@emotion/react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-
 function Navbar() {
+  const [clickState, setclickState] = useState(false)
+
+  const handleClick = () => {
+    if (clickState) {
+      setclickState(false);
+    } else {
+      setclickState(true);
+    }
+    console.log(clickState);
+  };
+
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to='/' className="flex items-center">
+        <Link to="/" className="flex items-center">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             className="h-8 mr-3"
@@ -16,6 +28,7 @@ function Navbar() {
           </span>
         </Link>
         <button
+          onClick={handleClick}
           data-collapse-toggle="navbar-default"
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -39,24 +52,27 @@ function Navbar() {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div className = {clickState ? "w-full md:block md:w-auto": "hidden w-full md:block md:w-auto"} id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link to="/"
+              <Link
+                to="/"
                 className="block py-2 pl-3 pr-4 text-blue bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
               >
                 Home
               </Link>
             </li>
             <li>
-              <Link to="/view-details"
+              <Link
+                to="/view-details"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 View Details
               </Link>
             </li>
             <li>
-              <Link to="/view-master"
+              <Link
+                to="/view-master"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Edit
